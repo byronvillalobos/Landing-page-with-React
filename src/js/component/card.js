47b5673e50677240a50868e92ae2405ba8e4b8ee
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-27 03:33:20 +0000

    comments

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -2,5 +2,17 @@ import React from "react";
 
 //create your first component
 export function Card() {
-	return <h1>Hello, world!</h1>;
+    return (
+        <div>
+            <div className="card" style="width: 18rem;">
+                <div className="card-body">
+                    <h5 className="card-title">Card title</h5>
+                    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the cards content.</p>
+                    <a href="#" className="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    );
+
 }
+
--- src/js/component/jumbotron.js
+++ src/js/component/jumbotron.js
@@ -2,5 +2,24 @@ import React from "react";
 
 //create your first component
 export function Jumbotron() {
-	return <h1>Hello, world!</h1>;
+	return (
+		<div>
+			<div className="jumbotron">
+				<h1 className="display-4">Hello, world!</h1>
+				<p className="lead">
+					This is a simple hero unit, a simple jumbotron-style
+					component for calling extra attention to featured content or
+					information.
+				</p>
+				<hr className="my-4"></hr>
+				<p>
+					It uses utility classNamees for typography and spacing to
+					space content out within the larger container.
+				</p>
+				<a className="btn btn-primary btn-lg" href="#" role="button">
+					Learn more
+				</a>
+			</div>
+		</div>
+	);
 }

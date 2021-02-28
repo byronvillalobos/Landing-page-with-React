b87074ba51cb6bcacc3e730454f2b15ccded7d6a
byronvillalobos <byronvillalobos@hotmail.com>
2021-02-28 21:42:49 +0000

    comments

--- src/js/component/card.js
+++ src/js/component/card.js
@@ -1,12 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 //create your first component
 export function Card(props) {
 	return (
 		<div className="col-sm-4" styles="width: 18rem;">
-			<img
-				src="https://i.pinimg.com/236x/32/7a/8d/327a8ddacdc5fa126f12f8d6990ab40d.jpg"
-				className="card-img-top"></img>
+			<img src={props.pic} className="card-img-top"></img>
 			<div className="card-body text-center">
 				<p className="card-text text-center">{props.name}</p>
 				<a href="#" className="btn btn-primary">
--- src/js/component/home.js
+++ src/js/component/home.js
@@ -13,9 +13,18 @@ export function Home() {
 			<Header />
 			<Jumbotron />
 			<div className="d-flex flex-row">
-				<Card name="1. Some quick example text to build on the card title and make up the bulk of the cards content." />
-				<Card name="2. Some quick example text to build on the card title and make up the bulk of the cards content." />
-				<Card name="3. Some quick example text to build on the card title and make up the bulk of the cards content." />
+				<Card
+					name="1. Some quick example text to build on the card title and make up the bulk of the cards content."
+					pic="https://i.pinimg.com/236x/32/7a/8d/327a8ddacdc5fa126f12f8d6990ab40d.jpg"
+				/>
+				<Card
+					name="2. Some quick example text to build on the card title and make up the bulk of the cards content."
+					pic="https://i.pinimg.com/236x/2c/d7/54/2cd7549e42230b4605070ef97f3af556.jpg"
+				/>
+				<Card
+					name="3. Some quick example text to build on the card title and make up the bulk of the cards content."
+					pic="https://i.pinimg.com/236x/4f/9a/66/4f9a66dfb868ba5e3ee465ac1b87f639.jpg"
+				/>
 			</div>
 			<Footer />
 		</div>
